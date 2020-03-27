3e87f0685cb8984761019255f6bef3035d6c4c20
danieldms <daniel.dms.23@gmail.com>
2020-03-26 23:54:48 -0300

    fix issue jwt strategy

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -12,12 +12,11 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 		super({
 			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 			ignoreExpiration: false,
-			passReqToCallback: true,
 			secretOrKey: SECRET
 		});
 	}
 
-	async validate(payload: any, done){
-		return done(null, payload);
+	async validate(payload: any){
+		return payload;
 	}
 }
